11e0ae90da8d715933c2f3feddb7ff699aa7280a
alantepox98 <alan.tepoxtecatl@gmail.com>
2021-10-14 17:37:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gram/ParseApplication.java
@@ -0,0 +1,15 @@
+package com.example.gram;
+
+import android.app.Application;
+
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+
+    }
+}
